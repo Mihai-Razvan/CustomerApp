8c3dd97b85fdf9117f8f5515d3348aad5dd4af89
Mihai Razvan <MIHAIRAZVAN03@YAHOO.COM>
2022-03-18 19:20:51 +0200

    .

--- CustomerApp/CustomerApp_Client/app/src/main/java/com/example/customerapp_client/MainActivity.java
+++ CustomerApp/CustomerApp_Client/app/src/main/java/com/example/customerapp_client/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.customerapp_client;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -18,24 +19,41 @@ import com.example.customerapp_client.databinding.ActivityMainBinding;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
     private Button act_main_home_button;
     private Button act_main_request_button;
+    private Button act_main_test_button;
+    private TextView act_main_tw;
+
+   // SQLiteDatabase database = openOrCreateDatabase("Users", MODE_PRIVATE, null);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        getActivityElements();
+        act_main_test_button_onClick();
     }
 
-    private void getActivityElements()
-    {
+    private void getActivityElements() {
         act_main_home_button = findViewById(R.id.act_main_home_button);
         act_main_request_button = findViewById(R.id.act_main_request_button);
+        act_main_tw = findViewById(R.id.act_main_tw);
+        act_main_test_button = findViewById(R.id.act_main_test_button);
+    }
+
+    private void act_main_test_button_onClick() {
+        act_main_test_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                act_main_tw.setText("Test");
+            }
+        });
     }
 }
