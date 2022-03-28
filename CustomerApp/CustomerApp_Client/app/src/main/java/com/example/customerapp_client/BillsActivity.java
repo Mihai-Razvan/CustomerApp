4805d35c5a946c112d7336704022dab7517e08b0
Mihai Razvan <MIHAIRAZVAN03@YAHOO.COM>
2022-03-29 00:36:55 +0300

    .

--- CustomerApp/CustomerApp_Client/app/src/main/java/com/example/customerapp_client/BillsActivity.java
+++ CustomerApp/CustomerApp_Client/app/src/main/java/com/example/customerapp_client/BillsActivity.java
@@ -7,20 +7,24 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.customerapp_client.databinding.ActivityBillsBinding;
 
 public class BillsActivity extends AppCompatActivity {
 
     private ActivityBillsBinding binding;
-    private TextView act_bills_bill_id_TW;
-    private TextView act_bills_bill_price_TW;
+    private ConstraintLayout act_bills_root_layout;
+    private RecyclerView act_bills_recyclerView;
+    private Button act_bills_add_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,12 +32,31 @@ public class BillsActivity extends AppCompatActivity {
         binding = ActivityBillsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-       // getActivityElements();
+        getActivityElements();
+        act_bills_add_button_onClick();
     }
 
     private void getActivityElements()
     {
-     //   act_bills_bill_id_TW = findViewById(R.id.act_bills_bill_id_TW);
-      //  act_bills_bill_id_TW = findViewById(R.id.act_bills_bill_price_TW);
+        act_bills_root_layout = findViewById(R.id.act_bill_root_layout);
+        act_bills_recyclerView = findViewById(R.id.act_bills_recyclerView);
+        act_bills_add_button = findViewById(R.id.act_bills_add_button);
+    }
+
+    private void act_bills_add_button_onClick()
+    {
+        act_bills_add_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addBillCard();
+            }
+        });
+    }
+
+    private void addBillCard()
+    {
+        View view = getLayoutInflater().inflate(R.layout.bill_card, null);
+
+        act_bills_recyclerView.addView(view);
     }
 }
